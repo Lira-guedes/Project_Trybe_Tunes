88ad7121b65fdce9dee85e38824fae102d730de4
Lira Guedes <liracrg@gmail.com>
2023-05-17 16:00:15 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -23,6 +24,11 @@ export default class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <p data-testid="header-user-name">{name}</p>
+        <ul>
+          <li><Link to="/search" data-testid="link-to-search">Search</Link></li>
+          <li><Link to="/favorites" data-testid="link-to-favorites">Favorites</Link></li>
+          <li><Link to="/profile" data-testid="link-to-profile">Profile</Link></li>
+        </ul>
       </header>
     );
   }
