f2132a8f2afccbb6ba60529248f6100b39a12f7d
Lira Guedes <liracrg@gmail.com>
2023-05-17 16:16:21 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,9 +2,40 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  state = {
+    disabledButton: true,
+  };
+
+  enableButton = ({ target }) => {
+    const min = 2;
+    const { name, value } = target;
+    if (value.length >= min) {
+      this.setState(() => ({
+        [name]: value,
+        disabledButton: false,
+      }));
+    }
+  };
+
   render() {
+    const { disabledButton } = this.state;
+
     return (
-      <div data-testid="page-search"><Header /></div>
+      <div data-testid="page-search">
+        <Header />
+        <div>
+          <input
+            data-testid="search-artist-input"
+            onChange={ this.enableButton }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ disabledButton }
+          >
+            Pesquisar
+          </button>
+        </div>
+      </div>
     );
   }
 }
