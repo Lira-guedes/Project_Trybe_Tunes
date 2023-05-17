855dbc56e2f8ffdb884a1285fbc8453c75c48cf5
Lira Guedes <liracrg@gmail.com>
2023-05-17 15:12:33 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends React.Component {
+  state = {
+    loading: false,
+    name: '',
+    redirect: false,
+    disabledButton: true,
+  };
+
+  enableButton = ({ target }) => {
+    const min = 3;
+    const { name, value } = target;
+    if (value.length >= min) {
+      this.setState(() => ({
+        [name]: value,
+        disabledButton: false,
+      }));
+    }
+  };
+
+  handleLogin = async () => {
+    const { name } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
+  };
+
   render() {
+    const { disabledButton, loading, redirect } = this.state;
+    if (redirect) return <Redirect to="/search" />;
+
     return (
-      <div data-testid="page-login">Login</div>
+      loading ? (<Loading />) : (
+        <div data-testid="page-login">
+          <form>
+            <label>
+              Nome:
+              <input
+                name="name"
+                data-testid="login-name-input"
+                placeholder="Digite o seu nome:"
+                onChange={ this.enableButton }
+              />
+            </label>
+            <button
+              data-testid="login-submit-button"
+              onClick={ this.handleLogin }
+              disabled={ disabledButton }
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+      )
     );
   }
 }
